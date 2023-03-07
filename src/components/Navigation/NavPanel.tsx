2ff8e8673e2549432b802a9a849d23690328b400
Dan Piths <danpiths@gmail.com>
2023-03-07 22:53:08 +0530

    corrected navpanel

--- src/components/Navigation/NavPanel.tsx
+++ src/components/Navigation/NavPanel.tsx
@@ -12,14 +12,14 @@ const NavPanel: FC<{
   const { data: session } = useSession();
 
   return (
-    <div className="absolute right-0 z-[70] flex h-screen w-10/12 flex-col bg-slate-800 drop-shadow-md">
+    <div className="fixed right-0 z-[70] flex h-screen w-10/12 flex-col bg-slate-800 drop-shadow-md">
       <button
         className="my-3 ml-auto mr-3 rounded-[0.05rem] focus:outline-none focus:ring-2 focus:ring-slate-200 focus:ring-offset-2 focus:ring-offset-slate-800"
         onClick={() => setIsPanelOpen(false)}
       >
         <XMarkIcon className="h-7 w-7" />
       </button>
-      <div className="flex h-full flex-col">
+      <div className="flex flex-col">
         {links.map((link, i) => (
           <Link
             key={i}
@@ -34,7 +34,7 @@ const NavPanel: FC<{
         ))}
         {session?.user ? (
           <button
-            className="primary-btn mx-4 mt-auto mb-4 bg-rose-400 hover:bg-rose-500 focus:bg-rose-500 focus:ring-rose-500 active:bg-rose-700 active:ring-rose-700"
+            className="primary-btn mx-4 mt-4 bg-rose-400 hover:bg-rose-500 focus:bg-rose-500 focus:ring-rose-500 active:bg-rose-700 active:ring-rose-700"
             // eslint-disable-next-line @typescript-eslint/no-misused-promises
             onClick={() => signOut({ callbackUrl: "/" })}
           >
